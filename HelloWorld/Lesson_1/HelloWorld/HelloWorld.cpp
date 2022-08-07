e8ae6dfd36648f30650f7241016eb2e75e6dca70
Yura Kuznetsov <yuriy.kuznetsov@nikel.com.ua>
2022-08-07 13:47:35 +0300

    Update lesson 1

---
+++ HelloWorld/Lesson_1/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,19 @@
+// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main() {
+    // console output: Hello World!
+    std::cout << "Hello World!\n";
+
+    // console output: Hello World!
+    std::cout << "Hello ";
+    std::cout << "World!\n";
+
+    // console output: Hello World!
+    std::cout << "Hello "
+        << "World!\n";
+
+    // statements above are equal
+}
